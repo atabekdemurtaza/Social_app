c6bdc19861bc2e6743fe1040b8e446dd47823960
atabekdemurtaza <mychannel21war@gmail.com>
2023-06-08 12:43:59 +0500

    Saved in June 08

---
+++ account/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    # Предыдущий url входа
+    #path('login/', views.user_login, name='login'),
+
+    # url-адреса входа и выхода
+    path('', include('django.contrib.auth.urls')),
+    path('', views.dashboard, name='dashboard')
+]
+
